581777978ad8545b2cd20ff7cfa7c46178e82a53
Hayden Housen <hayden@haydenhousen.com>
2024-04-01 17:44:56 -0700

    Update torch.py

--- deepspeed/comm/torch.py
+++ deepspeed/comm/torch.py
@@ -142,10 +142,9 @@ class TorchBackend(Backend):
         return self.reduce_scatter_function is not None
 
     def init_process_group(self, backend, timeout, init_method, rank, world_size):
-        from datetime import timedelta
         if not torch.distributed.is_initialized():
             torch.distributed.init_process_group(backend,
-                                                 timeout=timedelta(seconds=30*60),
+                                                 timeout=timeout,
                                                  init_method=init_method,
                                                  rank=rank,
                                                  world_size=world_size)
